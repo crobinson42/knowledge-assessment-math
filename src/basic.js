133a3618ded997f17f77194331254bacf9057d31
Cory Robinson <coryrobinson42@gmail.com>
2016-09-03 20:15:31 -0700

    add initial

---
+++ src/basic.js
@@ -0,0 +1,92 @@
+module.exports = {
+  0: {
+    question: '10 x 12',
+    answers: {
+      0: 22,
+      1: 120,
+      2: 112
+    },
+    answer: 1
+  },
+  1: {
+    question: '(11 + 3) x 2',
+    answers: {
+      0: 36,
+      1: 14,
+      2: 28
+    },
+    answer: 2
+  },
+  2: {
+    question: '50 &divide; 2',
+    answers: {
+      0: 25,
+      1: 50,
+      2: 20
+    },
+    answer: 0
+  },
+  3: {
+    question: '25 - 2 - 10',
+    answers: {
+      0: 15,
+      1: 11,
+      2: 13
+    },
+    answer: 2
+  },
+  4: {
+    question: '19 x 1',
+    answers: {
+      0: 20,
+      1: 19,
+      2: 0
+    },
+    answer: 1
+  },
+  5: {
+    question: '(41 - 4) + 16',
+    answers: {
+      0: 53,
+      1: 51,
+      2: 47
+    },
+    answer: 0
+  },
+  6: {
+    question: '28 + 13',
+    answers: {
+      0: 43,
+      1: 39,
+      2: 41
+    },
+    answer: 2
+  },
+  7: {
+    question: '15 &divide; 3',
+    answers: {
+      0: 5,
+      1: 18,
+      2: 45
+    },
+    answer: 0
+  },
+  8: {
+    question: '1013 + 12',
+    answers: {
+      0: 125,
+      1: 1001,
+      2: 1025
+    },
+    answer: 2
+  },
+  9: {
+    question: '5 x 8',
+    answers: {
+      0: 45,
+      1: 40,
+      2: 13
+    },
+    answer: 1
+  }
+}
